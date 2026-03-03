6691d71ce028ca1a44370c375262286e7b8ce0cf
LabibArefin123 <ah947777@gmail.com>
2026-03-03 16:58:44 +0600

    welcome page updated

--- resources/views/frontend/welcome.blade.php
+++ resources/views/frontend/welcome.blade.php
@@ -3,14 +3,10 @@
 @section('title', 'TechnoTech Engineering Ltd.')
 <link href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css" rel="stylesheet" />
 @section('content')
-    {{-- Header --}}
     @include('frontend.welcome_page.header')
-    {{-- Hero Banner --}}
     @include('frontend.welcome_page.banner')
-    {{-- About Lifeline City Hospital --}}
     @include('frontend.welcome_page.about')
     @include('frontend.welcome_page.services')
     @include('frontend.welcome_page.activities')
-    {{-- Footer --}}
     @include('frontend.welcome_page.footer')
 @endsection
