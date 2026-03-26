54d300253891becb90c6d9aca31dd72730a7412b
LabibArefin123 <ah947777@gmail.com>
2026-03-27 00:39:50 +0600

    fixed login part

--- public/js/custom_frontend/open_quote_modal.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var offcanvasElement = document.getElementById("quoteOffcanvas");
-    var offcanvas = new bootstrap.Offcanvas(offcanvasElement);
-
-    document.getElementById("openQuote").addEventListener("click", function () {
-        offcanvas.show();
-    });
-});
